2ef2286059229af81d7cd52e796d07bf968ab9f1
thebogie <thebogie@live.com>
2023-12-26 23:12:49 -0600

    production ready testing part 3

--- front/src/lib/interfaces/global.d.ts
+++
@@ -1,7 +0,0 @@
-/* declare module 'svelte-cookie' {
-    import { CookieSerializeOptions, CookieAttributes } from 'cookie';
-
-    export function getCookie(name: string): string | undefined;
-    export function setCookie(name: string, value: string, options?: CookieSerializeOptions & CookieAttributes): void;
-    //export function delete(name: string): void;
-}*/
